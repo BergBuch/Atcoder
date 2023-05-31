0926ef53cd6e0492f6c8f0fb0632f6c93bb34e0d
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-01 04:11:01 +0900

    abc303 e AC!, import abc302

---
+++ abc302/B/main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<long long>>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define sayYes cout<<"Yes"<<endl;
+#define sayNo cout<<"No"<<endl;
+int main(){
+    int h,w;
+    cin >> h >> w;
+    vector<string> s(h);
+    rep(i,0,h)cin>>s[i];
+    
+}
