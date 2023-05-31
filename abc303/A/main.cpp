d98b9d66f3431e35eb28e252e6d875177bac60f5
BurgBuch <yamamoto2000227@yahoo.co.jp>
2023-05-31 21:36:58 +0900

    abc303 b

--- abc303/A/main.cpp
+++ abc303/A/main.cpp
@@ -1,4 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n;
+    string s,t;
+    cin >> n;
+    cin >> s >> t;
+    for (int i=0;i<n;i++){
+        if(s[i]=='o')s[i]='0';
+        if(s[i]=='l')s[i]='1';
+        if(t[i]=='o')t[i]='0';
+        if(t[i]=='l')t[i]='1';
+    }
+    if(s==t)cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
--- abc303/B/main.cpp
+++ abc303/B/main.cpp
@@ -1,4 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n,m;
+    cin >> n >> m;
+    int a[100][100];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin >> a[i][j];
+        }
+    }
+    int f[100][100];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<n+1;j++){
+            f[i][j]=0;
+        }
+    }
+    int x,y;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n-1;j++){
+            x=a[i][j];
+            y=a[i][j+1];
+            f[x][y]=1;
+            f[y][x]=1;
+        }
+    }
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        for(int j=i+1;j<=n;j++){
+            if(f[i][j]==1) ans++;
+        }
+    }
+    cout << ans << endl;
 }
