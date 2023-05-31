9748f855c490db8139ea8874f5d55a8ba26ca835
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-05-31 21:58:54 +0900

    abc303 b AC!

--- abc303/B/main.cpp
+++ abc303/B/main.cpp
@@ -27,7 +27,7 @@ int main(){
     int ans=0;
     for(int i=1;i<=n;i++){
         for(int j=i+1;j<=n;j++){
-            if(f[i][j]==1) ans++;
+            if(f[i][j]==0) ans++;
         }
     }
     cout << ans << endl;
