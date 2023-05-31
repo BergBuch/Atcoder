8f0d8a3393bc52e501058d5a8b20d1fa93d5bcdd
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-01 01:04:59 +0900

    abc303 cd AC!

--- abc303/C/main.cpp
+++ abc303/C/main.cpp
@@ -1,4 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n,m,h,k;
+    cin >> n >> m >> h >> k;
+    string s;
+    cin >> s;
+    set<pair<int,int>> p;
+    for(int i=0;i<m;i++){
+        int x,y;
+        cin >> x >> y;
+        p.insert({x,y});
+    }
+    pair<int,int> t(0,0);
+    for(int i=0;i<n;i++){
+        if(s[i]=='R'){
+            t.first++;
+        }
+        if(s[i]=='L'){
+            t.first--;
+        }
+        if(s[i]=='U'){
+            t.second++;
+        }
+        if(s[i]=='D'){
+            t.second--;
+        }
+        h--;
+        if(h<0){
+            cout << "No" << endl;
+            return 0;
+        }
+        if(h<k and p.count({t.first,t.second})){
+            h=k;
+            p.erase({t.first,t.second});
+        }
+    }
+    cout << "Yes" << endl;
 }
--- abc303/D/main.cpp
+++ abc303/D/main.cpp
@@ -1,4 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    long long x,y,z;
+    cin >> x >> y >> z;
+    string s;
+    cin >> s;
+    int n = s.size();
+    long long r[n+1][2];
+    r[0][0] = 0;
+    r[0][1] = z;
+    for(int i=1;i<=n;i++){
+        if(s[i-1]=='a'){
+            r[i][0] = min(r[i-1][0] + x, r[i-1][1]+ z + x);
+            r[i][1] = min(r[i-1][0] + z + y, r[i-1][1] + y);
+        }
+        if(s[i-1]=='A'){
+            r[i][0] = min(r[i-1][0] + y, r[i-1][1]+ z + y);
+            r[i][1] = min(r[i-1][0] + z + x, r[i-1][1] + x);
+        }
+    }
+    long long ans = min(r[n][0],r[n][1]);
+    cout << ans << endl;
 }
