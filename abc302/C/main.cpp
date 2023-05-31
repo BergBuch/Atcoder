6101b79b93fd14ee7a22486cef7e6f4213c3635c
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-01 05:05:43 +0900

    abc302 c AC!

--- abc302/C/main.cpp
+++ abc302/C/main.cpp
@@ -6,10 +6,28 @@ using vi = vector<int>;
 using vll = vector<long long>;
 using vvi = vector<vector<int>>;
 using vvll = vector<vector<long long>>;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
-#define sayYes cout<<"Yes"<<endl;
-#define sayNo cout<<"No"<<endl;
+#define sayYes cout<<"Yes"<<endl
+#define sayNo cout<<"No"<<endl
 int main(){
-    
+    int n,m;
+    cin>>n>>m;
+    vector<string> s(n);
+    rep(i,0,n) cin>>s[i];
+    bool ans=false;
+    sort(s.begin(),s.end());
+    do{
+        bool all_neighbor = true;
+        rep(i,0,n-1){
+            int same=0;
+            rep(j,0,m) if(s[i][j]==s[i+1][j]) same++;
+            if(same!=m-1) all_neighbor=false;
+        }
+        if(all_neighbor){
+            ans=true;
+            break;
+        }
+    }while(next_permutation(s.begin(),s.end()));
+    YesNo(ans);
 }
