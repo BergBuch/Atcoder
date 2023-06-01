a1d0893a0e4cbdee041f87862a1a041b327eccb3
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-01 22:01:26 +0900

    abc301 D AC!

--- abc301/D/main.cpp
+++ abc301/D/main.cpp
@@ -11,5 +11,27 @@ using vvll = vector<vector<long long>>;
 #define sayYes cout<<"Yes"<<endl;
 #define sayNo cout<<"No"<<endl;
 int main(){
-    
+    string s;
+    cin >> s;
+    ll n;
+    cin >> n;
+
+    ll blist[60];
+    blist[0] = 1;
+    rep(i,1,60) blist[i] = blist[i-1]*2;
+
+    ll ans_num=0;
+    for(int i=s.size()-1;i>=0;i--) if(s[i]=='1') ans_num+=blist[s.size()-1-i];
+
+    if(ans_num>n){
+        cout << -1 << endl;
+        return 0;
+    }
+
+    rep(i,0,s.size()){
+        if(s[i]=='?'){
+            if(ans_num+blist[s.size()-1-i]<=n) ans_num += blist[s.size()-1-i];
+        }
+    }
+    cout << ans_num << endl;
 }
