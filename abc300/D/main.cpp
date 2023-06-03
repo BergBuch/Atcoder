18f47f5a42c679f4906e07d400790a3efa4b9b04
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-03 19:50:55 +0900

    import abc300

---
+++ abc300/D/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<long long>>;
+#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define sayYes cout<<"Yes"<<endl;
+#define sayNo cout<<"No"<<endl;
+int main(){
+    
+}
