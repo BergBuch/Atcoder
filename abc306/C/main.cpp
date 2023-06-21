c65f0b3f1c4b71a96a9781c4be44adbae3a25b58
BurgBuch <yamamoto2000227@yahoo.co.jp>
2023-06-21 16:40:52 +0900

    ABC306 C AC!

--- abc306/C/main.cpp
+++ abc306/C/main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin >> n;
+    int a;
+    vector<int> sorted_index;
+    vector<int> count(n+1,0);
+    for(int i=0; i<3*n; i++){
+        cin >> a;
+        count[a]++;
+        if(count[a]==2) sorted_index.push_back(a);
+        if(sorted_index.size()==n) break;
+    }
+    for(int i=0; i<n-1; i++) cout << sorted_index[i] << " ";
+    cout << sorted_index[n-1] << endl;
 }
