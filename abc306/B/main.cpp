836865cbe10306f3da136152c7a9df0e759e706c
BurgBuch <yamamoto2000227@yahoo.co.jp>
2023-06-21 16:31:17 +0900

    ABC306 B AC!

--- abc306/B/main.cpp
+++ abc306/B/main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
-    
+    unsigned long long ans=0;
+    for(int i=0; i<64; i++){
+        unsigned long long b;
+        cin >> b;
+        ans += (b << i);
+    }
+    cout << ans << endl;
 }
