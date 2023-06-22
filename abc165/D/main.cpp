7382d978ccf1e88c956287456479112167c45845
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-22 19:54:23 +0900

    ABC165 D AC!

--- abc165/D/main.cpp
+++ abc165/D/main.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int a;
+long long b,n;
+long long f(long long x){
+    return (a*x)/b - a*(x/b);
+}
+
 int main(){
-    
+    cin >> a >> b >> n;
+    long long ans = max(f(n),f((n/b)*b - 1));
+    cout << ans << endl;
 }
