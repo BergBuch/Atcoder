21d9f5b65f3ce96ef34e5090a08c0fcbedf95d3c
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-22 09:14:25 +0900

    ABC305 E solving

--- abc305/E/main.cpp
+++ abc305/E/main.cpp
@@ -1,16 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using vi = vector<int>;
-using vll = vector<long long>;
-using vvi = vector<vector<int>>;
-using vvll = vector<vector<long long>>;
-#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
-#define repa(e,a) for(auto e:(a))
-#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
-#define sayYes cout<<"Yes"<<endl;
-#define sayNo cout<<"No"<<endl;
+vector<vector<int>> graph;
+
 int main(){
+    //グラフの情報を入力
+    int N,M,K;
+    cin >> N >> M >> K;
+    graph.resize(N+1);
+    vector<int> is_guarded(N+1,-1);
+
+    for(int i=0; i<M; i++){
+        int a,b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    //警備員情報から幅優先探索をしていく
+    for(int i=0; i<K; i++){
+        int p,h;
+        cin >> p >> h;
+        is_guarded[p] = h;
+    }
+    
     
 }
