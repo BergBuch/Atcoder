16607e135a687597df396854f7496dfec393d509
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-22 20:07:19 +0900

    ABC008 B AC!

--- abc008/B/main.cpp
+++ abc008/B/main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin >> n;
+    map<string,int> vote;
+    for(int i=0; i<n; i++){
+        string s;
+        cin >> s;
+        if(vote.find(s) != vote.end()){
+            vote[s]++;
+        }
+        else{
+            vote[s]=1;
+        }
+    }
+    string ans_name;
+    int max_vote=0;
+    for(auto e: vote){
+        if(max_vote<e.second){
+            max_vote = e.second;
+            ans_name = e.first;
+        }
+    }
+    cout << ans_name << endl;
 }
