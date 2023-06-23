29ce850c5034e2f154f1d836baf28d6bb70d33c6
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-23 10:41:32 +0900

    AC!

--- abc037/C/main.cpp
+++ abc037/C/main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
-    
+    int N,K;
+    cin >> N >> K;
+    vector<long long> s(N+1,0);
+    for(int i=1; i<=N; i++){
+        long long a;
+        cin >> a;
+        s[i] = s[i-1] + a;
+    }
+    long long ans=0;
+    for(int i=K; i<=N; i++){
+        ans += s[i] - s[i-K];
+    }
+    cout << ans << endl;
 }
