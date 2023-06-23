b2b5c7d6703f3db25f0fc34c1da9c2f38918688d
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-23 10:30:32 +0900

    AC!

--- abc174/C/main.cpp
+++ abc174/C/main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
-    
+    long long k;
+    cin >> k;
+    long long sevens = 0;
+    for(int i=1; i<=10000000; i++){
+        sevens = sevens*10 + 7;
+        sevens %= k;
+        if(sevens == 0){
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
 }
