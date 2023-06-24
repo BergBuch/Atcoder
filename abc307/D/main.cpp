d34d0ae48f65814fe00ffaf840bc9d4c9d6d636b
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-24 21:59:07 +0900

    AC!

--- abc307/D/main.cpp
+++ abc307/D/main.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main(){
-    
+    int N;
+    std::cin >> N;
+    string S;
+    std::cin >> S;
+
+    stack<int> st;
+    vector<int> status(N,-1);
+    for(int i=0; i<N; i++){
+        if(S[i]=='('){
+            st.push(i);
+        }
+        if(S[i]==')'){
+            if(st.empty()) continue;
+            status[st.top()] = i;
+            st.pop();
+        }
+    }
+    for(int i=0; i<N; i++){
+        if(status[i]==-1){
+            std::cout << S[i];
+        }
+        else{
+            i=status[i];
+        }
+    }
+    std::cout << endl;
 }
