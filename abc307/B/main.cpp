9f216ace83157088d4c59d1716c51fec53100d30
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-24 21:21:12 +0900

    AC!

--- abc307/B/main.cpp
+++ abc307/B/main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main(){
-    
+    //input
+    int N;
+    std::cin >> N;
+    vector<string> s(N);
+    for(int i=0; i<N; i++) std::cin >> s[i];
+
+    //search
+    string ans="No";
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            if(i==j) continue;
+            string cs = s[i] + s[j];
+            bool is_cycle = true;
+            for(int k=0; k<=(cs.size()-1)/2; k++){
+                if(cs[k]!=cs[cs.size()-1-k]){
+                    is_cycle=false;
+                    break;
+                }
+            }
+            if(is_cycle) ans = "Yes";
+        }
+    }
+    std::cout << ans << endl;
 }
