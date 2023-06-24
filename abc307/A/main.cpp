c7988a9858f1c8b39c488f1a9b579a01a2ba764f
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-06-24 21:11:02 +0900

    AC!

--- abc307/A/main.cpp
+++ abc307/A/main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-    
+    int N;
+    string ans;
+    std::cin >> N;
+    for(int i=0; i<N; i++){
+        int sum=0;
+        for(int j=0;j<7; j++){
+            int a;
+            std::cin >> a;
+            sum += a;
+        }
+        if(i==N-1) ans += to_string(sum);
+        else ans += to_string(sum) + " ";
+    }
+    std::cout << ans << endl;
 }
