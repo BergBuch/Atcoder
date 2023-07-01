a3cd0fbf53b1f1e699549a4d0314131ccbb9d488
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-01 21:15:12 +0900

    AC!

--- abc308/B/main.cpp
+++ abc308/B/main.cpp
@@ -2,5 +2,38 @@
 using namespace std;
 
 int main(){
-    
+    int N,M;
+    cin >> N >> M;
+    vector<string> c(N);
+    vector<string> d(M);
+    vector<int> p(M);
+
+    //入力
+    int init_price;
+    for(int i=0; i<N; i++){
+        cin >> c[i];
+    }
+    for(int i=0; i<M; i++){
+        cin >> d[i];
+    }
+    cin >> init_price;
+    for(int i=0; i<M; i++){
+        cin >> p[i];
+    }
+
+    map<string,int> price;
+    for(int i=0; i<M; i++){
+        price[d[i]] = p[i];
+    }
+
+    //計算
+    int ans=0;
+    for(int i=0; i<N; i++){
+        if(price.find(c[i])!=price.end()){
+            ans += price[c[i]];
+        }
+        else ans += init_price;
+    }
+
+    cout << ans << endl;
 }
