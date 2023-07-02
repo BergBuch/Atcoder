f3f5123c1e22cc72f492517f9eae1aa210ccd941
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-02 20:59:36 +0900

    not complete yet

--- abc308/C/main.cpp
+++ abc308/C/main.cpp
@@ -1,16 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
     //入力
     int N;
     cin >> N;
     multimap<double,int> mp; //p[probability] = player_iterate
     for(int i=1; i<=N; i++){
-        int a,b;
+        long long a,b;
         cin >> a >> b;
-        double probability = double(a)/double(a+b);
-        mp.insert({1-probability,i});
+        double probability = 1-double(a)/double(a+b);
+        mp.insert({probability,i});
     }
 
     //出力
