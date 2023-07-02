d1af4a32e56c226c87cff2d7c9ba62489059b2a2
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-02 21:18:56 +0900

    AC!

--- arc163/A/main.cpp
+++ arc163/A/main.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string getAns(int N, string S){
+    string ans = "No";
+    for(int i=0; i<N-1; i++){
+        string former = S.substr(0,i+1);
+        string latter = S.substr(i+1);
+        if(former<latter) ans = "Yes";
+    }
+    return ans;
+}
+
 int main(){
-    
+    int T;
+    cin >> T;
+    for(int i=0; i<T; i++){
+        int N;
+        string S;
+        cin >> N >> S;
+        cout << getAns(N,S) << endl;
+    }
 }
