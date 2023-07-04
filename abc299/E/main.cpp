daf26666b3651796189abeac8ca4e0b519f4b036
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-04 21:32:14 +0900

    ac!

--- abc299/E/main.cpp
+++ abc299/E/main.cpp
@@ -16,10 +16,12 @@ int main(){
     vector<bool> is_black(N+1,true);
     int K;
     cin >> K;
+    vector<pair<int,int>> cons(K);
 
     for(int i=0; i<K; i++){
         int p,dist;
         cin >> p >> dist;
+        cons[i] = {p,dist};
         if(dist==0)continue;
 
         queue<pair<int,int>> q;
@@ -40,15 +42,49 @@ int main(){
         }
     }
 
-    for(int i=1; i<=N; i++) if(is_black[i]){
-        cout << "Yes" << endl;
-        for(int i=1; i<=N; i++){
-            if(is_black[i]) cout << 1;
-            else cout << 0;
+    string ans = "Yes";
+
+    for(int i=0; i<K; i++){
+        int p,dist;
+        p = cons[i].first;
+        dist = cons[i].second;
+
+        if(dist==0 && !is_black[p]){
+            ans = "No";
+            break;
         }
+        
+        queue<pair<int,int>> q;
+        q.push(make_pair(p,0));
+        vector<bool> is_searched(N+1,false);
+        bool found_black = false;
+        while(!q.empty()){
+            int now_node = q.front().first;
+            int now_dist = q.front().second;
+            q.pop();
+            if(now_dist==dist){
+                if(is_black[now_node]) found_black = true;
+                continue;
+            }
+            else{
+                for(auto e: g[now_node]){
+                    if(is_searched[e]) continue;
+                    q.push(make_pair(e,now_dist+1));
+                    is_searched[e] = true;
+                }
+            }
+        }
+        if(!found_black){
+            ans = "No";
+            break;
+        }
+    }
+    if(ans=="Yes"){
+        cout << ans << endl;
+        for(int i=1; i<=N; i++) cout << is_black[i];
         cout << endl;
-        return 0;
-    } 
-    cout << "No" << endl;
-    return 0;
+    }
+    else{
+        cout << ans << endl;
+    }
 }
