f83dbf0efd983be1fff9a10fb99126d3e42c6e04
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-04 10:58:07 +0900

    ac!

--- abc299/C/main.cpp
+++ abc299/C/main.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main(){
-    
+    int N;
+    string S;
+    cin >> N >> S;
+
+    if(string(N,'o')==S){
+        cout << -1 << endl;
+        return 0;
+    }
+
+    int ans=0;
+    int now_combo = 0;
+    for(auto s: S){
+        if(s=='-'){
+            ans = max(ans,now_combo);
+            now_combo=0;          
+        }
+        if(s=='o') now_combo++;
+    }
+    ans = max(ans,now_combo);
+    if(ans==0) ans = -1;
+    cout << ans << endl;
 }
