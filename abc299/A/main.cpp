222da42171ca6a39effe2ad154686f477ea96aa1
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-04 10:34:28 +0900

    AC!

--- abc299/A/main.cpp
+++ abc299/A/main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main(){
-    
+    int N;
+    string S;
+    cin >> N >> S;
+
+    string ans = "out";
+    int state = 0;
+    for(int i=0; i<N; i++){
+        if(state==0){
+            if(S[i]=='|') state++;
+        }
+        else if(state==1){
+            if(S[i]=='|') state++;
+            if(S[i]=='*') ans = "in";
+        }
+    }
+    cout << ans << endl;
 }
