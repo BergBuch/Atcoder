dc8d0d3d36af495a4b6b12c34bbf3dd485541660
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-09 21:09:25 +0900

    ac!

--- arc164/A/main.cpp
+++ arc164/A/main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
-    
+    int T;
+    cin >> T;
+    for(int q=0; q<T; q++){
+        long long N,K;
+        cin >> N >> K;
+        long long num_bit=0;
+        while(N!=0){
+            num_bit += N%3;
+            N /= 3;
+        }
+        if(num_bit<=K && (num_bit-K)%2==0) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
 }
