1f7705d199a440d2259f8721cbea8aed8971ec68
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-09 23:15:37 +0900

    not complete

--- arc164/D/main.cpp
+++ arc164/D/main.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+long long mod = 998244353;
+
 int main(){
+    long long N;
+    string T;
+    cin >> N >> T;
+
+    vector<int> e(2*N);
+    for(int i=0; i<2*N; i++){
+        if(T[i]=='+') e[i] = 1;
+        if(T[i]=='-') e[i] = -1;
+        if(T[i]=='?') e[i] = 0;
+    }
+
     
 }
