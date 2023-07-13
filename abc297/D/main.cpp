a5ed62275f21df8c8fd48cfacc5166bf79c58946
BurgBuch <yamamoto2000227@yahoo.co.jp>
2023-07-13 15:26:34 +0900

    ac!

--- abc297/D/main.cpp
+++ abc297/D/main.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main(){
-    
+    long long A,B;
+    cin >> A >> B;
+    long long ans = 0;
+    while(A!=B){
+        if(A>B){
+            ans += A/B;
+            if(A%B==0){
+                A = B;
+                ans--;
+                continue;
+            }
+            A = A%B;
+        }
+        else if(B>A){
+            ans += B/A;
+            if(B%A==0){
+                B = A;
+                ans--;
+                continue;
+            }
+            B = B%A;
+        }
+    }
+    cout << ans << endl;
 }
