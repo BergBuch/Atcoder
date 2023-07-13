458c64acc78014ef627e714af637757bc45cdc77
BurgBuch <yamamoto2000227@yahoo.co.jp>
2023-07-13 15:40:57 +0900

    ac!

--- abc297/E/main.cpp
+++ abc297/E/main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main(){
-    
+    int N,K;
+    cin >> N >> K;
+
+    set<long long> P;
+    vector<long long> A(N);
+    for(int i=0; i<N; i++){
+        cin >> A[i];
+        P.insert(A[i]);
+    }
+
+    for(int k=0; k<K-1;k++){
+        long long cheap = *P.begin();
+        P.erase(P.begin());
+        for(int i=0; i<N; i++) P.insert(cheap+A[i]);
+    }
+
+    cout << *P.begin() << endl;
 }
