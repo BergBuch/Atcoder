34973a5f571edd906db47a3982be4f006fc9f82b
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-15 07:09:24 +0900

    ac

--- dp/I/main.cpp
+++ dp/I/main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main(){
-    
+    int N;
+    cin >> N;
+    vector<long double> p(N+1);
+    for(int i=1;i<=N;i++) cin >> p[i];
+
+    vector<vector<long double>> dp(N+1,vector<long double>(N+1,0));
+    dp[0][0]=1;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<=i;j++){
+            dp[i+1][j+1] += dp[i][j]*p[i+1];
+            dp[i+1][j] += dp[i][j]*((long double)1-p[i+1]);
+        }
+    }
+    long double ans=0;
+    for(int i=N/2+1;i<=N;i++){
+        ans += dp[N][i];
+    }
+    cout << setprecision(11) << ans << endl;
 }
