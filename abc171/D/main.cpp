3cb2e3831e7dba2b34cfffb36b3ce776710fec5d
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-14 22:16:10 +0900

    ac!

--- abc171/D/main.cpp
+++ abc171/D/main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main(){
-    
+    int N;
+    cin >> N;
+    vector<int> A(100001,0);
+    long long sum=0;
+    for(int i=0;i<N;i++){
+        int a;
+        cin >> a;
+        sum += a;
+        A[a]++;
+    }
+
+    int Q;
+    cin >> Q;
+    for(int q=0;q<Q;q++){
+        int b,c;
+        cin >> b >> c;
+        int count = A[b];
+        A[b] = 0;
+        A[c] += count;
+        sum = sum + count*(c-b);
+
+        cout << sum << endl;
+    }
 }
