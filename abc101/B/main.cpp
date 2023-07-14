d033fb381f48381d85bc2ab4d14a15c5f7e4a96d
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-14 21:53:55 +0900

    ac!

--- abc101/B/main.cpp
+++ abc101/B/main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    long long N;
+    cin >> N;
+    int sn=0;
+    int tn=N;
+    while(tn>0){
+        sn += tn%10;
+        tn /= 10;
+    }
+    if(N%sn==0) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
