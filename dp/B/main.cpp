be20a1765422f2ea5db86e6b3ea975209004ecbe
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-15 02:29:54 +0900

    ac

--- dp/B/main.cpp
+++ dp/B/main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main(){
+    int N,K;
+    cin >> N >> K;
+    vector<int> h(N);
+    for(auto &x: h) cin >> x;
     
+    vector<long long> dp(N,LLONG_MAX);
+    dp[0] = 0;
+
+    for(int i=0;i<N-1;i++){
+        for(int k=1;k<=K;k++){
+            if(i+k>N-1) continue;
+            dp[i+k] = min(dp[i+k],dp[i]+abs(h[i]-h[i+k]));
+        }
+    }
+
+    cout << dp[N-1] << endl;
 }
