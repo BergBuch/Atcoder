6524be07575b307edf2f02ef3093160f104d11f6
BergBuch <yamamoto2000227@yahoo.co.jp>
2023-07-14 22:06:11 +0900

    ac!

--- abc187/C/main.cpp
+++ abc187/C/main.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main(){
+    int N;
+    cin >> N;
+    bool is_satis = true;
+    string ans;
+    set<string> d;
+    vector<string> S(N);
+    for(int i=0;i<N;i++) cin >> S[i];
+
+    for(int i=0;i<N;i++){
+        if(S[i][0]!='!'){
+            d.insert(S[i]);
+        }
+    }
+    for(int i=0;i<N;i++){
+        if(S[i][0]=='!'){
+            S[i] = S[i].substr(1);
+            if(d.find(S[i])!=d.end()){
+                is_satis = false;
+                ans = S[i];
+                break;
+            }
+        }
+    }
     
+    if(is_satis) cout << "satisfiable" << endl;
+    else cout << ans << endl;
 }
